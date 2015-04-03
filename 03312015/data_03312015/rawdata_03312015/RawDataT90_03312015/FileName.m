c422eee8e43cc3235e6e6901e52067cfe02eb7f4
Yijun Jiang <yjjiang@mit.edu>
2015-04-03 00:09:15 -0400

    changed file ordering

--- 03312015/data_03312015/rawdata_03312015/RawDataT90_03312015/FileName.m
+++
@@ -1,3 +0,0 @@
-fileFolder = fullfile('/afs/athena.mit.edu/user/k/a/kakkarav/Desktop/QIP/03312015/Data_03312015/RawData_03312015/RawDataT90_03312015');
-dirOutput = dir(fullfile(fileFolder, '*.mat'));
-fileNames = {dirOutput.name};
