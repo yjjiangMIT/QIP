ecba061695fbd9634bc9fee2a5b35376848812e7
Yijun Jiang <yjjiang@mit.edu>
2015-04-10 10:33:33 -0400

    cleaning up

--- Scripts/New/firstRunMe.m
+++
@@ -1,1 +0,0 @@
-global calib PC0H PC1H PC0C PC1C b1H b2H b1C b2C;
--- Scripts/New/test.m
+++
@@ -1,6 +0,0 @@
-spect = cn;
-pvh = exp(1i*(PC0H+PC1H*spect.hfreq/(spect.hfreq(end)-spect.hfreq(1)))*pi/180);
-spectCorrected = spect;
-spectCorrected.hspect = spect.hspect.*pvh;
-figure;
-plot(spectCorrected.hfreq, real(spectCorrected.hspect));
