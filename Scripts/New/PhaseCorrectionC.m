e45d433289d2c079bf1a031930cbbedc7a4eda78
Yijun Jiang <yjjiang@mit.edu>
2015-04-10 16:06:24 -0400

    code

---
+++ Scripts/New/PhaseCorrectionC.m
@@ -0,0 +1,12 @@
+% Does phase corrections for carbon, then redo peak integrals.
+
+% Phase corrections
+pvc = exp(1i*(PC0C+PC1C*spectTemp.cfreq/(spectTemp.cfreq(end)-spectTemp.cfreq(1)))*pi/180);
+spectTemp.cspect = pvc.*spectTemp.cspect;
+
+% Redo peak integrals
+pf = evalin('base', 'calib.pf');
+iw = evalin('base', 'calib.iwidth');
+
+cpeak = do_integral(spectTemp.cfreq, spectTemp.cspect, pf, iw, spectTemp.csfo);
+spectTemp.cpeaks = cpeak(3:4);
